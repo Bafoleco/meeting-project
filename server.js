dc893f1e2055967d9d0b0ac2ae835acdce83d9be
Bafoleco <bayfoleycox@gmail.com>
2018-01-29 12:46:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ var session      = require('express-session');
 // mongoose.connect("mongodb://heroku_zn4rp3qx:unsecure1@ds117868.mlab.com:17868/heroku_zn4rp3qx");
 require('./config/passport')(passport);
 
-// app.set('port', process.env.PORT);
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
